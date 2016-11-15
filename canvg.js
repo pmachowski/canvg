eefebebffb40163f83effba96a465c19fd1360a4
Peter Machowski <piotr@machowski.co>
2016-11-15 15:20:50 +0100

    Update

--- canvg.js
+++ canvg.js
@@ -10,16 +10,6 @@
 
 	'use strict';
 
-	// export as AMD...
-	if ( typeof define !== 'undefined' && define.amd ) {
-		define('canvgModule', [ 'rgbcolor', 'stackblur' ], factory );
-	}
-
-	// ...or as browserify
-	else if ( typeof module !== 'undefined' && module.exports ) {
-		module.exports = factory( require( 'rgbcolor' ), require( 'stackblur' ) );
-	}
-
 	global.canvg = factory( global.RGBColor, global.stackBlur );
 
 }( typeof window !== 'undefined' ? window : this, function ( RGBColor, stackBlur ) {
